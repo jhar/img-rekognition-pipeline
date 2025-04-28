e36589bc946419fa0ac431f623aa14b9035d1beb
jhar <jhar-dev@protonmail.com>
2025-04-27 19:16:42 -0700

    force_destroy on bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,8 @@ locals {
 
 # Bucket for storing images
 resource "aws_s3_bucket" "this" {
-  bucket = "${local.prefix}-bucket"
+  bucket        = "${local.prefix}-bucket"
+  force_destroy = true
 }
 
 # DynamoDB table for storing image attributes
@@ -82,7 +83,7 @@ resource "aws_iam_policy" "this" {
         Resource = ["${aws_s3_bucket.this.arn}"]
       },
       {
-        Action = ["dynamodb:PutItem"],
+        Action   = ["dynamodb:PutItem"],
         Effect   = "Allow"
         Resource = ["${aws_dynamodb_table.this.arn}"]
       }
